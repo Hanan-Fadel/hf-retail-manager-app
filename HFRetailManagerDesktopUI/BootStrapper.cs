d7eea8884a250d53488cc8677083a0e77992f264
Hanan Fadel <hfadel@ellerca.com>
2022-03-08 15:38:28 -0500

    Create WPF Login Form

--- HFRetailManagerDesktopUI/BootStrapper.cs
+++ HFRetailManagerDesktopUI/BootStrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using HFRetailManagerDesktopUI.Helpers;
 using HFRetailManagerDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace HFRetailManagerDesktopUI
 {
@@ -17,6 +19,12 @@ namespace HFRetailManagerDesktopUI
         public BootStrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
+
         }
 
         protected override void Configure()
--- HFRetailManagerDesktopUI/ViewModels/ShellViewModel.cs
+++ HFRetailManagerDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace HFRetailManagerDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+
+        //Constructor injection to pass in a new instance of loginVM
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            //Give me a new LoginViewModel instance
+            _loginVM = loginVM;
+
+            //Showme the View corresponsing to this LoginViewModel which is LoginView
+            ActivateItemAsync(_loginVM);
+        }
 
     }
 }
