ecf3f0006d593b72a1d2946d8d6ece41ea288809
Hanan Fadel <hfadel@ellerca.com>
2022-03-08 22:16:18 -0500

    Fetch User data from database using the API.

---
+++ HF-RetailManager-WebAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using HFRetailManagerDataManagerClassLibrary.DataAccess;
+using HFRetailManagerDataManagerClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using System.Web;
+
+namespace HF_RetailManager_WebAPI.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            return data.GetUserById(userId);
+        }
+
+    }
+}
---
+++ HFRetailManagerDataManagerClassLibrary/DataAccess/UserData.cs
@@ -0,0 +1,26 @@
+﻿using HFRetailManagerDataManagerClassLibrary.Internal.DataAccess;
+using HFRetailManagerDataManagerClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFRetailManagerDataManagerClassLibrary.DataAccess
+{
+    public class UserData
+    {
+        //Get infomation from User table
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id }; //Anonymous object
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "HFConnectionString");
+
+            return output;
+        }
+
+    }
+}
